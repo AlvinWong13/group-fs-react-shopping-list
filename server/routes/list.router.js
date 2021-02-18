014760878a5e9c7353e17defb933ad8e1f50d8ed
George Papagapitos <george.g.papagapitos@gmail.com>
2021-02-18 10:46:59 -0600

    submitting post route for pr

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -3,6 +3,24 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
+router.post('/', function (req, res) {
+  console.log('inside SERVER-Side-Post');
+  console.log('req.body', req.body);
 
+  const foodItem = req.body;
+  const queryText =
+    'INSERT INTO "shopping_list" ("name", "quantity", "unit") VALUES ($1, $2, $3)';
+
+  pool
+    .query(queryText, [req.body.name, req.body.quantity, req.body.unit])
+    .then(function (dbResults) {
+      console.log('results of dbResults', dbResults);
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log('error in POST', error);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 
-import Header from '../Header/Header.jsx'
+import Header from '../Header/Header.jsx';
 import './App.css';
 
-
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <main>
-                <p>Under Construction...</p>
-            </main>
-        </div>
-    );
+  const addFoodItem = () => {
+    axios
+      .post('/list', {
+        name: foodItemName,
+        quantity: foodItemQuantity,
+        unit: foodItemUnit,
+      })
+      .then((response) => {
+        getShoppingList();
+      })
+      .catch((error) => {
+        console.log('error posting', error);
+      });
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <p>Under Construction...</p>
+      </main>
+    </div>
+  );
 }
 
 export default App;
