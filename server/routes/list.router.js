554d439563607d3e7fe6ae992eca10cb9109dfc6
Alvin Wong <wong.alvin13@gmail.com>
2021-02-18 10:34:55 -0600

    GET reqeust updated

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -4,5 +4,17 @@ const pool = require('../modules/pool.js');
 
 // TODO - Add routes here...
 
+// GET route
+router.get('/', (req, res) => {
+  const sqlText = `SELECT * FROM shopping_list ORDER BY name ASC`;
+  pool.query(sqlText)
+  .then(results => {
+    res.send(results.row);
+  })
+  .catch(err => {
+    console.log('Error contacting database', err);
+    res.sendStatus(500);
+  });
+});
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
+
 function App() {
+
+// setShoppingList
+let [shoppingList, setShoppingList] = useState([]);
+
+// on load get list
+useEffect(() => {
+    getShoppingList()
+}, [])
+
+const getShoppingList = () => {
+    axios.get('/list')
+        .then(response => {
+            console.log('shopping list data', response.data);
+            setShoppingList(response.data);
+        })
+        .catch(err => {
+            alert('Unable to get shopping list');
+            console.log('error getting shopping list', err)
+        })
+}
+
     return (
         <div className="App">
             <Header />
